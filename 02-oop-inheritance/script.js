ab3a4999cb1ba41aa8ff457588979965c63a4c8a
Guillermo Maiolo <guille.maiolo@gmail.com>
2015-11-07 19:02:53 -0300

    Points 11-12

--- 02-oop-inheritance/script.js
+++ 02-oop-inheritance/script.js
@@ -1,21 +1,21 @@
 function Movie(){}
 
 Movie.prototype.attributes = {
-  title : null,
-  year : null,
-  actors : null
+  title: null,
+  year: null,
+  actors: []
 }
-Movie.prototype.play = function(){
+Movie.prototype.play = function() {
   MovieObserver.notify(this, 'play');
 }
-Movie.prototype.stop = function(){
+Movie.prototype.stop = function() {
   MovieObserver.notify(this, 'stop');
 }
-Movie.prototype.set = function(attr, value){
-  this.attributes.attr = value;
+Movie.prototype.set = function(attr, value) {
+  this.attributes[attr] = value;
   console.log(attr + " set to " + value);
 }
-Movie.prototype.get = function(attr){
+Movie.prototype.get = function(attr) {
   console.log("Retrieved " + attr);
   return this.attributes.attr;
 }
@@ -30,7 +30,7 @@ DownloadableMovie.prototype.download = function(){
 }
 
 var MovieObserver = {
-  notify : function(movie, fn){
+  notify : function(movie, fn) {
     switch(fn){
       case 'play' : {
         console.log("Playing " + movie.attributes.title);
@@ -41,4 +41,24 @@ var MovieObserver = {
       }
     }
   }
+}
+
+function Actor(name) {
+  this.name = name
+}
+
+var george = new Actor("George Clooney");
+var morgan = new Actor("Morgan Freeman");
+var johnny = new Actor("Johnny Depp");
+
+var movieStars = [george, morgan, johnny];
+
+var movie = new Movie();
+movie.set('title', "someTitle");
+
+movie.attributes.actors = movie.attributes.actors.concat(movieStars);
+var movieActors = movie.attributes.actors;
+
+for(actor in movieActors){
+ console.log(movieActors[actor].name);
 }
